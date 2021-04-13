1b1870b8c3622df455b469ddbfdaf6f3873109b8
caiquetgr <caiquetgr@gmail.com>
2021-04-13 18:51:53 -0300

    Added Assembly Atari2600 course

---
+++ assembly/atari2600_assembly/cleanmem/cleanmem.asm
@@ -0,0 +1,36 @@
+    processor 6502
+
+    seg code
+    org $F000       ; Define the code origin at $F000
+
+Start:
+    sei             ; Disable interrupts
+    cld             ; Disable the BCD decimal math mode
+    ldx #$FF        ; Loads the X register with $FF
+    txs             ; Transfer the X register value  to the (S)tack pointer
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Clear the Page Zero region ($00 to $FF)
+;;; Meaning the entire RAM and also the entire TIA registers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    lda #0          ; A = 0
+    ldx #0        ; X = #$FF
+
+MemLoop:
+    dex             ; X--
+    sta $0,X        ; Store the value of A inside memory address $0 + X
+    bne MemLoop     ; Loop until X is equal to zero (z flag is set)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Fill the ROM size to exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    org $FFFC
+    .word Start     ; Reset vector at $FFFC (where the program starts)
+    .word Start     ; Interrupt vector at $FFFE (unused in the VCS)
+
+; compile: dasm cleanmem.asm -f3 -v0 -ocat.bin
+
+; LDA #80 (immediate mode)
+; LDA $80 (absolute (zero page) mode)
---
+++ assembly/atari2600_assembly/colorbg/colorbg.asm
@@ -0,0 +1,25 @@
+    processor 6502
+
+    include "vcs.h"
+    include "macro.h"
+
+    seg code
+    org $F000       ; defines the origin of the ROM at $F000
+
+START:
+    CLEAN_START     ; Macro to safely clear the memory
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;   Set background luminosity color to yellow
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    lda #$1E        ; Load color into A ($1E is NTSC yellow)
+    sta COLUBK      ; Store A to BackgroundColor Address $09
+
+    jmp START       ; Repeat from Start
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;   Fill ROM size to exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    org $FFFC       ; Define to origin $FFFC
+    .word START     ; reset vector at $FFFC (where the program starts)
+    .word START     ; Interrupt vector at $FFFE (unused in VCS)
